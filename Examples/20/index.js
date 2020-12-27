9677ad82b25d33aeeddf5e699e647185afe28cb8
iamovsyanka <anakstkv@gmail.com>
2020-12-27 15:40:07 +0300

    feat: implement 20 example

---
+++ Examples/20/index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+    if (request.method === 'POST') {
+        let body = '';
+        request.on('data', (chunk) => { body += chunk; });
+        request.on('end', () => {
+            try {
+                //Метод JSON.parse() разбирает строку JSON,
+                // возможно с преобразованием получаемого в процессе разбора значения.
+                let parm = JSON.parse(body);
+                if (parm.login === 'Test' && parm.password === 'test') {
+                    response.end('Status: OK.');
+                } else {
+                    response.end('Status: not OK.');
+                }
+            }
+            catch (e) {
+                console.error(e);
+                response.end('I can see that it\'s not JSON')
+            }
+        });
+    }
+}).listen(5002);
+
+console.log('Server running at http://localhost:5002/');
