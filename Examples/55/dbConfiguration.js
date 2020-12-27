3b2c1881a3d6a0edf03a9f2fe6bdac131c9b89da
iamovsyanka <anakstkv@gmail.com>
2020-12-28 00:43:42 +0300

    feat: implement 55 example

---
+++ Examples/55/dbConfiguration.js
@@ -0,0 +1,12 @@
+const config = {
+    user: 'NodeJS',
+    password: 'NodeJS',
+    server: 'localhost',
+    database: 'Univer',
+    "options": {
+        "encrypt": true,
+        "enableArithAbort": true
+    }
+};
+
+module.exports = config;
