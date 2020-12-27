a5e8ef11ef9bc5d3f1bf3cb2c7d25f06af07393e
iamovsyanka <anakstkv@gmail.com>
2020-12-28 00:49:54 +0300

    feat: implement 56 example

---
+++ Examples/56/index.js
@@ -0,0 +1,25 @@
+const DB = require('./DB');
+const http = require('http');
+const url = require('url');
+const Db = new DB();
+
+let http_handler = (request, response) => {
+    if (request.method === 'PUT' && url.parse(request.url).pathname === '/faculties') {
+        let data = '';
+        request.on('data', chunk => {
+            data += chunk;
+        });
+        request.on('end', () => {
+            data = JSON.parse(data);
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            Db.putFaculties(data.faculty, data.faculty_name).then(records => {
+                response.end(JSON.stringify(data))
+            }).catch(error => { console.error(error) });
+        });
+    }
+};
+
+let server = http.createServer();
+server.listen(3002, () => {
+    console.log('server.listen(3002)')
+}).on('request', http_handler);
