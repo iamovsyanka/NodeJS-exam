d4852be6459db3c36fcc2be11495979b546b9b86
iamovsyanka <anakstkv@gmail.com>
2020-12-28 02:56:16 +0300

    feat: implement 34 example

---
+++ Examples/34/server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const ws = new WebSocket.Server({
+    port:5000,
+    host:'localhost'
+});
+
+ws.on('connection', (ws) => {
+//Сообщения Pong автоматически отправляются в ответ на сообщения ping,
+// как того требует спецификация.
+    ws.on('pong', (data) => {
+        console.log('on pong: ', data.toString());
+    });
+
+    setInterval(() => {
+        console.log('server: ping');
+        ws.ping('server: ping')
+    }, 1000);
+});
+
+ws.on('error', (e) => { console.log('error', e) });
