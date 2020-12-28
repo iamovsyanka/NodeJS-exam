ebe515a9466309acba2c4dde3754729b0673fbba
iamovsyanka <anakstkv@gmail.com>
2020-12-28 03:37:08 +0300

    refactor: add comments

--- Examples/38/client.js
+++ Examples/38/client.js
@@ -2,12 +2,16 @@ const rpcWSC = WebSocket =  require('rpc-websockets').Client;
 let ws = new rpcWSC('ws://localhost:4000');
 
 ws.on('open', () => {
+    //Вызывает зарегистрированный метод RPC на сервере. Решает, как только ответ готов.
     ws.call('kek').then((r) => { console.log('message = ', r); });
     ws.call('sum', [2, 4, 6, 8, 10]).then((r) => { console.log('sum = ', r); });
     ws.call('secret').catch((err) => { console.error(err) }).then((r) => { console.log('date = ', r); })
+    //Логины с другой стороны соединения
+    // Параметры используются для аутентификации с другой стороны соединения и определяются пользователем.
+    // Бросает с подробным сообщением, если вход в систему не удается.
     ws.login({login: 'admin', password: 'admin'})
         .then((login) => {
             ws.call('secret').then((r) => { console.log('date = ', r); })
        }
-    )
+    );
 });
--- Examples/38/server.js
+++ Examples/38/server.js
@@ -1,10 +1,20 @@
+/*Библиотека rpc-websockets позволяет разработчикам легко реализовать свою бизнес-логику,
+включающую обмен сообщениями между пользователями, машинами или любыми устройствами.
+Он предоставляет возможность отправлять и получать данные JSON через коммуникационный
+протокол WebSocket для поддержки двухстороннего уведомления push, запуска методов RPC
+и запуска любых типов сигнализации событий. Только клиенты могут вызывать методы RPC,
+а не наоборот в данный момент.*/
 const rpcWSS = require('rpc-websockets').Server;
 let server = new rpcWSS({port: 4000, host:'localhost'});
 
 server.setAuth(credentials => credentials.login === 'admin' && credentials.password === 'admin');
 
+//Регистрирует метод RPC и возвращает объект RPCMethod для управления разрешениями метода.
 server.register('kek', () => 'Kek').public();
 
+//Помечает метод RPC как общедоступный. Все клиенты, как аутентифицированные,
+// так и анонимные, смогут использовать этот метод.
+// Этот параметр установлен по умолчанию в файле .register.
 server.register('sum', (params) => {
     let sum = 0;
     params.forEach(function (item, i, params){
@@ -14,4 +24,6 @@ server.register('sum', (params) => {
     return sum;
 }).public();
 
+//Помечает метод RPC как защищенный. Этот метод будет доступен только в том случае,
+// если клиент успешно прошел аутентификацию с помощью файла .login.
 server.register('secret', () => new Date().toISOString()).protected();
